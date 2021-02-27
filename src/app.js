6776fabcb91ea58fb3ae1c84a7588e18e25bcd3b
melcong <77306721+melcong@users.noreply.github.com>
2021-02-26 21:11:33 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ function formatDate(timestamp){
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute(
         "src",
-        `  <i class="fas fa-cloud-sun"></i>`);
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
         iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
